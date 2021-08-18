425e4012f9e11a8ac258d35eb63b8a47430487b9
NukaGrizz <nukagrizz@gmail.com>
2021-08-17 22:55:11 -0400

    add render badge function

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,11 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-//function renderLicenseBadge(license) {
- // if (!license) {
- //   return '';
-//  } else {}
-
-///}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+  switch(license) {
+      case 'MIT License':
+        licenseBadge = "[![MIT License](https://img.shields.io/badge/License-MIT-blue)](https://choosealicense.com/licenses/mit/)"
+        return licenseBadge;
+      case 'Mozilla Public License 2.0':
+        licenseBadge = "[![Mozilla Public License 2.0 License](https://img.shields.io/badge/License-Mozilla%20Public%20License%202.0-orange)](https://choosealicense.com/licenses/mpl-2.0/)"
+        return licenseBadge;
+      case 'Apache License 2.0':
+        licenseBadge = "[![Apache License 2.0](https://img.shields.io/badge/License-Apache%20License%202.0-red)](https://choosealicense.com/licenses/apache-2.0/)"
+        return licenseBadge;
+      case 'GNU AGPLv3':
+        licenseBadge = "[![GNU AGPLv3 License](https://img.shields.io/badge/License-GNU%20AGPLv3-brightgreen)](https://choosealicense.com/licenses/agpl-3.0/)"
+        return licenseBadge;
+      case 'GNU GPLv3':
+        licenseBadge = "[![GNU GPLv3 License](https://img.shields.io/badge/License-GNU%20GPLv3-green)](https://choosealicense.com/licenses/gpl-3.0/)"
+        return licenseBadge;
+      case 'GNU LGPLv3':
+        licenseBadge = "[![GNU LGPLv3 License](https://img.shields.io/badge/License-GNU%20LGPLv3-yellowgreen)](https://choosealicense.com/licenses/lgpl-3.0/)"
+        return licenseBadge;
+      case 'Boost Software License 1.0':
+        licenseBadge = "[![Boost Software License 1.0](https://img.shields.io/badge/License-Boost%20Software%20License%201.0-yellow)](https://choosealicense.com/licenses/bsl-1.0/)"
+        return licenseBadge;
+      case 'The Unlicense':
+        licenseBadge = "[![Unlicense License 1.0]https://img.shields.io/badge/License-The%20Unlicense-lightgrey)](https://choosealicense.com/licenses/unlicense/"
+        return licenseBadge;
+    };
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -49,7 +75,7 @@ module.exports = data => {
   
   # ${data.title}
 
-  ${renderlicenseBadge(data.license)}
+  ${renderLicenseBadge(data.license)}
   
   ## Description 
 
